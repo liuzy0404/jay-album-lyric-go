ba75efe845532aaaf67c97ecf1d17fd20f7ac93b
liuzy0404 <mostic1122@gmail.com>
2017-05-17 12:26:37 +0800

    Add export comment

--- request/request.go
+++ request/request.go
@@ -14,42 +14,41 @@ var (
 	albumExp = regexp.MustCompile(`<textarea\s+style="display:none;">(.*)<\/textarea>`)
 	lyricExp = regexp.MustCompile(`\[.*\]`)
 	ua       = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.36 Safari/537.36"
-	lyricPAI = "http://music.163.com/api/song/media?id="
+	lyricAPI = "http://music.163.com/api/song/media?id="
+	// AlbumAPI export albumApi
+	AlbumAPI = "http://music.163.com/album?id="
 )
 
 type song struct {
 	Name string
-	Id   int
+	ID   int
 }
 
 type lyric struct {
 	Lyric string
 }
 
-func HttpGet(url, albumName string) (content string, statuscode int) {
+// HTTPGet export HTTPGet func
+func HTTPGet(url, albumName string) (content string) {
 	var songList []song
 	res, err := http.Get(url)
 	if err != nil {
 		fmt.Println("http get error.")
-		statuscode = -100
 		return
 	}
 	defer res.Body.Close()
 	data, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		fmt.Println("http read error.")
-		statuscode = -200
 		return
 	}
-	statuscode = res.StatusCode
 	// get matched array
 	submatch := albumExp.FindSubmatch([]byte(data))
 	content = string(submatch[1])
 	json.Unmarshal([]byte(content), &songList)
 	for _, song := range songList {
-		//fmt.Println(song.Name, "'s id is ", song.Id)
-		fmt.Println("[Get] " + lyricPAI + strconv.Itoa(song.Id))
-		getLyric(lyricPAI+strconv.Itoa(song.Id), albumName, song.Name)
+		//fmt.Println(song.Name, "'s id is ", song.ID)
+		getLyric(lyricAPI+strconv.Itoa(song.ID), albumName, song.Name)
 	}
 	return
 }
@@ -85,4 +84,5 @@ func getLyric(url, albumName, songName string) {
 		return
 	}
 	file.Write([]byte(lyricContent.Lyric))
+	fmt.Println(songName + " saved!")
 }
