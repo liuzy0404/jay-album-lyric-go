d1b573e671f81157888f7433325085a902afd02e
liuzy0404 <mostic1122@gmail.com>
2017-05-17 12:39:16 +0800

    Add user support

--- main.go
+++ main.go
@@ -2,10 +2,42 @@ package main
 
 import (
 	"fmt"
+	"jay-album-lyric-go/albumId"
 	"jay-album-lyric-go/request"
 )
 
+func getUserTips(slice []albumId.Album) (showUserTips string) {
+	for _, album := range slice {
+		showUserTips += "[" + album.ID + "] - " + album.AlbumName + "\n"
+	}
+	return showUserTips
+}
+
+func containsString(slice []albumId.Album, input string) bool {
+	for _, album := range slice {
+		if input == album.ID {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
-	request.HttpGet("http://music.163.com/album?id=18918", "jay")
-	fmt.Println("ok")
+	var input string
+	userTips := getUserTips(albumId.AlbumList)
+	userTips += "Enter a album's id: "
+	fmt.Print(userTips)
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if containsString(albumId.AlbumList, input) {
+		album := albumId.GetAlbumID(input)
+		request.HTTPGet(request.AlbumAPI+album.AlbumID, album.AlbumName)
+		fmt.Println("done!")
+		return
+	}
+	fmt.Println("\nYou should enter a correct album's id")
+	main()
 }
